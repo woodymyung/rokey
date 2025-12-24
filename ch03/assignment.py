773c10b02b862bae9f2e64e15b00ff21a38c8877
woodymyung <woody.myung@gmail.com>
2025-12-24 11:33:55 +0900

    파일 정리

--- ch03/assignment.py
+++
@@ -1,9 +0,0 @@
-if True :
-      if False:
-          print("1")
-          print("2")
-      else:
-          print("3")
-else :
-    print("4")
-print("5")
--- turtle_2.py
+++
@@ -1,8 +0,0 @@
-import random 
-
-
-animals = ['giraffe', 'monkey', 'rabbit']
-
-print(random.choice(animals))
-print(random.sample(animals, 2))
-print(random.randint(0, 100))
